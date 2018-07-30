107789c8df6ad65aec20075b9115b0e34f461913
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2018-07-30 17:27:37 -0400

    MVC Brad Traversy

---
+++ Udemy/BenjiTraMVC/app/bootstrap.php
@@ -0,0 +1,5 @@
+<?php 
+// charge les librairies
+require_once 'libraries/Core.php';
+require_once 'libraries/Controller.php';
+require_once 'libraries/Database.php';
---
+++ Udemy/BenjiTraMVC/public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+  require_once '../app/bootstrap.php';
+
+  //Initialisation de nouveau libraire
+  $init = new Core;
+
+  
+
