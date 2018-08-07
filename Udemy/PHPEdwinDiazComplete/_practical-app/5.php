143e9945a84d0ec5eee00576400869268943099f
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2018-08-07 11:36:42 -0400

    practice 5

--- Udemy/PHPEdwinDiazComplete/_practical-app/5.php
+++ Udemy/PHPEdwinDiazComplete/_practical-app/5.php
@@ -1,30 +1,39 @@
 <?php include "functions.php" ?>
 <?php include "includes/header.php" ?>
-	<section class="content">
+<section class="content">
 
-		<aside class="col-xs-4">
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+  <aside class="col-xs-4">
+    <?php Navigation();?>
 
 
-<article class="main-content col-xs-8">
-
-	
-	<?php 
+  </aside>
+  <!--SIDEBAR-->
 
 
-/*  Step1: Use a pre-built math function here and echo it
+  <article class="main-content col-xs-8">
 
 
-	Step 2:  Use a pre-built string function here and echo it
+    <?php 
 
+//Step1: Use a pre-built math function here and echo it
 
-	Step 3:  Use a pre-built Array function here and echo it
+$math = 4.8;
+ 
+    echo floor($math);
+    
+ echo   rand (15, 19);
 
- */
+	//Step 2:  Use a pre-built string function here and echo it
+$test = "Bonjour a tous et a toutes";
+  
+ echo "<br>";
+    echo strlen($test);
 
+	//Step 3:  Use a pre-built Array function here and echo it
+$arr = [95, 85, 935, 753, 154, 1, 254, 5];
+    echo "<br>";
+    
+    echo max($arr);
 	
 ?>
 
@@ -32,5 +41,6 @@
 
 
 
-</article><!--MAIN CONTENT-->
-<?php include "includes/footer.php" ?>
+  </article>
+  <!--MAIN CONTENT-->
+  <?php include "includes/footer.php" ?>
