9c37b60e6fc4064da4514802c376e202ce395a42
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2018-08-11 12:21:46 -0400

    practical 6

--- Udemy/PHPEdwinDiazComplete/_practical-app/6.php
+++ Udemy/PHPEdwinDiazComplete/_practical-app/6.php
@@ -12,14 +12,41 @@
 
 
 <article class="main-content col-xs-8">
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
  
+  <form action="6.php" method=POST>
+    <input type="text" name="user">
+    <input type="password" name="pass">
+    <input type="submit" name="submit" placeholder="Send me home">
+    
+  </form>
+
+
+
+</body>
+</html>
 
 	<?php  
+  
+ 
 
-/*  Step1: Make a form that submits one value to POST super global
-
-
- */
+/*  Step1: Make a form that submits one value to POST super global */
+ if (isset($_POST["submit"])) {
+   $user = $_POST["user"];
+   echo "Ton nom est: " ."'".$user."'";
+   echo "<br>";
+   $pass = $_POST["pass"];
+   echo "Ton mot de passe est: " ."'".$pass."'";
+   
+ }
+  
 
 	
 ?>
